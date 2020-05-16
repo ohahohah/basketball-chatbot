74758603a6106ae3ad5dbd999d56f2f34d586072
JJososo <atcsy89@gmail.com>
2020-05-16 12:53:58 +0900

    수정 1. find / selet 등의 함수 테스트 삭제 2. find 를 이용하여 print 해보기 3. select를 이용하여 csv 파일 만들어 보기

--- scraping_wkblranking/scraping_wkblranking.py
+++ scraping_wkblranking/scraping_wkblranking.py
@@ -11,34 +11,47 @@ html = req.text
 # BeautifulSoup으로 html소스를 python객체로 변환하기
 soup = BeautifulSoup(html, 'html.parser')
 
+# 여기까지는 Fix
+
 # 테이블 가져오기
-# select 는 여러 개의 인스턴스를 찾아 목록을 반환하고 find 는 첫 번째를 찾음. select_one 은 find 와 같다.
-# find, find_all, select, select_one 비교
-# 근데 find_all 하고 findAll 은 또 뭐가 다를까
+table = soup.find(id='regularTeamRecordList_table')
+table_csv = soup.select('#regularTeamRecordList_table')
+# print(tables)
 
-# table = soup.find('#regularTeamRecordList_table')
-# print(table)
-# 결과값이 NONE
 
-# table = soup.find_all('#regularTeamRecordList_table')
-# print(table)
-# 결과값이 빈 리스트
+# 내용을 행별로 row로 구분
+row = table.find_all('tr')
+# print(row)
 
-table = soup.select('#regularTeamRecordList_table')
-# print(table)
-# 결과값이 html 형식. text만 추출하고 싶은데...
+# row 내용을 cell로 받기
+for cell in row:
+    ranking_team = []
+    result_team = []
+    for ranking in cell.find_all('th'):
+        ranking_team.append(ranking.get_text())
+    for result in cell.find_all('td'):
+        result_team.append(result.get_text())
+    print(ranking_team, result_team)
+    print(ranking_team, result_team[0])
+# th, td를 따로 가져오는 것은 의미가 없나?
 
-# table = soup.select_one('#regularTeamRecordList_table')
-# print(table)
-# select와 같은 결과. 왜 똑같지? select_one과 find가 같다고 했는데...
-
+# csv 파일로 저장
 csvFile = open("wkblranking.csv", 'wt', newline='', encoding='utf-8')
 writer = csv.writer(csvFile)
+
 try:
-    for row in table:
+    for row_csv in table_csv:
         csvRow = []
-        for cell in row.findAll(['td', 'th']):
+        for cell in row_csv.findAll(['td', 'th']):
             csvRow.append(cell.get_text())
         writer.writerow(csvRow)
 finally:
     csvFile.close()
+
+# print 되게 작성한 부분과 csv로 작성된 부분을 하나로 합칠 수 있을까?
+# print는 find로 가져와서 test 형식으로 가능한데, csv는 select로 가져와야 가능하던데...
+# print와 csv로 나누는 건 의미가 없나? csv로 저장만 하면 되려나?
+
+# csv 파일의 행 바꿈은 어떻게 할 수 있을까?
+# 기록을 딕셔너리로 넣을 수 있을까?
+# 쳇봇에게 csv가 좋을까? 딕셔너리가 좋을까?
